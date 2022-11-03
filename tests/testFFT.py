224366d044b0d3eedea231cc56749d68c5a752e7
CristianRodrigoGuarachiIbanez <cristguarachi@gmail.com>
2022-11-03 10:00:31 +0100

    unittest

--- tests/testFFT.py
+++ tests/testFFT.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from rs_pcl_photo2.mk_normals import FFT
 from rs_pcl_photo2.lib.depth_image import image_to_array_2d, calc_normals_from_depth
 
+
 class TestFFT(unittest.TestCase):
     def reset(self):
         self._fft = FFT(self._read())
@@ -13,6 +14,7 @@ class TestFFT(unittest.TestCase):
     def reconstruct_image(self):
         new_fft = self._fft.reconstruct_image(file=None, filter=None, mask=None, method=1, freq=0.499211)
         assert new_fft.shape == self._fft.shape
+
 
 if __name__ == "__main__":
     pass
